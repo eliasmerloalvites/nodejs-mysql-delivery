7bec9eb89af4a72343b7c45ef9454061508bcda8
elias.merlo <elias.merlo@grupotawa.com>
2023-06-24 18:22:06 -0500

    actualizando consultas

--- controllers/ordersController.js
+++ controllers/ordersController.js
@@ -71,10 +71,10 @@ module.exports = {
             }
 
             for (const d of data) {
-                d.address = JSON.parse(d.address);
-                d.client = JSON.parse(d.client);
-                d.delivery = JSON.parse(d.delivery);
-                d.products = JSON.parse(d.products);
+                d.address = JSON.parse(JSON.stringify(d.address));
+                d.client = JSON.parse(JSON.stringify(d.client));
+                d.delivery = JSON.parse(JSON.stringify(d.delivery));
+                d.products = JSON.parse(JSON.stringify(d.products));
             }
             
             
